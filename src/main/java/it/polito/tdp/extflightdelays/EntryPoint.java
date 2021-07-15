447b82cc88943f332ae6be5010bf5759fbad38ff
Davieko <s260527@studenti.polito.it>
2021-07-16 01:42:57 +0200

    Fatto

--- src/main/java/it/polito/tdp/extflightdelays/EntryPoint.java
+++ src/main/java/it/polito/tdp/extflightdelays/EntryPoint.java
@@ -32,7 +32,6 @@ public class EntryPoint extends Application {
      * main() serves only as fallback in case the application can not be
      * launched through deployment artifacts, e.g., in IDEs with limited FX
      * support. NetBeans ignores main().
-     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
